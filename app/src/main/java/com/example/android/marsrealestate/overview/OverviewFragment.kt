771a98a8881462569ea4e616001f4b6efb0f3246
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2021-01-26 17:02:42 -0300

    fail internet images added

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -40,15 +40,23 @@ class OverviewFragment : Fragment() {
      * Inflates the layout with Data Binding, sets its lifecycle owner to the OverviewFragment
      * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
      */
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
         val binding = FragmentOverviewBinding.inflate(inflater)
+        //val binding = GridViewItemBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
+        binding.photosGrid.adapter = PhotoGridAdapter()
+
+        viewModel.coroutinesFunction()
 
         setHasOptionsMenu(true)
         return binding.root
--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -32,14 +32,20 @@ import java.lang.Exception
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData String that stores the most recent response
-    private val _response = MutableLiveData<String>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
-    // The external immutable LiveData for the response String
-    val response: LiveData<String>
-        get() = _response
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
+
+    private val _status = MutableLiveData<MarsApiStatus>()
+
+    val status: LiveData<MarsApiStatus>
+        get() = _status
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -54,15 +60,13 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsRealEstateProperties() {
 
         viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
             try {
-                val listResult = MarsApi.retrofitService.getProperties()
-                _response.value =
-                        "Success: ${listResult.size} Mars properties retrieved"
-            }
-            catch(e:Exception) {
-                _response.value = "Failure: ${e.message}"
+                _properties.value = MarsApi.retrofitService.getProperties()
+                _status.value = MarsApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
             }
         }
-
     }
 }
